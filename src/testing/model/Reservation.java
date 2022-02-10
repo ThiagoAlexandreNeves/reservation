bbea7395b16e0bb4b916a9a3e763d1be9f44eb45
ThiagoAlexandreNeves <thiago,alexandre.ac@gmail.com>
2022-02-10 10:07:34 -0300

    registro melhor validado

--- src/testing/model/Reservation.java
+++ src/testing/model/Reservation.java
@@ -7,34 +7,38 @@ import java.util.concurrent.TimeUnit;
 import testing.exception.DomainException;
 
 public class Reservation {
-	
+
 	SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-	
+
 	private int number;
 	private Date checkIn;
 	private Date checkOut;
-	
+
 	public Reservation(int number, Date checkIn, Date checkOut) throws DomainException {
-		if(!checkOut.after(checkIn)) {
-			throw new DomainException("out antes do in");
+		Date now = new Date();
+		if (!checkOut.after(checkIn)) {
+			throw new DomainException("CheckOut não é uma data futura ao checkIn");
+		}
+		if (checkIn.before(now) || checkOut.after(now)) {
+			throw new DomainException("Data anterior a atual");
 		}
 		this.number = number;
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
 	}
-	
+
 	public void update(Date checkIn, Date checkOut) throws DomainException {
 		Date now = new Date();
-		if(!checkOut.after(checkIn)) {
-			throw new DomainException("out antes do in");
+		if (!checkOut.after(checkIn)) {
+			throw new DomainException("CheckOut não é uma data futura ao checkIn");
 		}
-		if(checkIn.before(now) || checkOut.after(now)) {
-			throw new DomainException("Datas passadas");
+		if (checkIn.before(now) || checkOut.after(now)) {
+			throw new DomainException("Data anterior a atual");
 		}
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
 	}
-	
+
 	public long duration() {
 		long diff = checkOut.getTime() - checkIn.getTime();
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
@@ -42,10 +46,8 @@ public class Reservation {
 
 	@Override
 	public String toString() {
-		return "Reservation: number " + number + ", "
-				+ "checkIn " + checkIn + ", "
-				+ "checkOut " + checkOut + ", "
-				+ duration() + " nights";
+		return "Reservation: number " + number + ", " + "checkIn " + checkIn + ", "
+				+ "" + "checkOut " + checkOut + ", " + duration() + " nights";
 	}
 
 	public int getNumber() {
